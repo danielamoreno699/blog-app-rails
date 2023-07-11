0331136da3c38b4e9be86f58ea4ef62f78f6d43a
Daniela Moreno <dani9292@hotmail.com>
2023-07-11 13:21:12 -0500

    run migration

--- db/migrate/20230711180807_create_user.rb
+++ db/migrate/20230711180807_create_user.rb
@@ -1,6 +1,10 @@
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :Name
+      t.string :Photo
+      t.string :Bio
+      t.integer :PostCounter
 
       t.timestamps
     end
