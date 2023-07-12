195a3bb44e82aa50a2a2f93dcfcc60ac118df816
Daniela Moreno <dani9292@hotmail.com>
2023-07-12 14:41:56 -0500

    insert increment/decrement counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,18 @@ class Post < ApplicationRecord
     belongs_to :user, foreign_key: 'author_id'
     has_many :likes
     has_many :comments
+
+    after_create :increment_post_counter
+    after_destroy :decrement_post_counter
+  
+    private
+
+    def increment_post_counter
+        puts 'Incrementing post counter'
+        user.increment!(:post_counter)
+    end
+    def decrement_post_counter
+        puts 'Decrementing post counter'
+        user.decrement!(:post_counter)
+    end
 end
