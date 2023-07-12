9dee033fef5055ebee2b88417c985a31225c5086
Daniela Moreno <dani9292@hotmail.com>
2023-07-12 13:42:05 -0500

    implement method on user rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :user, foreign_key: :author_id
+    belongs_to :user, foreign_key: 'author_id'
     has_many :likes
     has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :posts
     has_many :likes
 
-    def recent_posts
-    end
+    def recent_posts(limit)
+        posts.order(created_at: :desc).limit(limit)
+      end
+      
 end
