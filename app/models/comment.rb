7c7f1935a87bed8ad71be79ffcb063a5f09ac511
Daniela Moreno <dani9292@hotmail.com>
2023-07-13 10:29:52 -0500

    Insert validations for comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
+
+  validates :author_id, presence: true
+  validates :post_id, presence: true
+  validates :Text, presence: true
+
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'post_id'
 
