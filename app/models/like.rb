7c6e1c9281c49d394e95a7bf62ed87ed8ba2c2a2
Daniela Moreno <dani9292@hotmail.com>
2023-07-13 11:15:55 -0500

    Insert post_counter validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,5 @@
 class Like < ApplicationRecord
 
-  validates :author_id, presence: true
-  validates :post_id, presence: true
-
   belongs_to :user, foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'post_id'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
 
   
   validates :name, presence: true
-  validates :photo, presence: true
-  validates :bio, presence: true
+  validates :post_counter, comparison: { greater_than_or_equal_to: 0 }
 
 
   has_many :comments, foreign_key: 'author_id'
