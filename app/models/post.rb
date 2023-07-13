4307f8c1d23fb4b26668bcd525856155327103a7
Daniela Moreno <dani9292@hotmail.com>
2023-07-13 11:22:24 -0500

    add  validations for  counter and likes counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
 
   validates :author_id, presence: true
-  validates :Title, presence: true
-  validates :Text, presence: true
-
+  validates :Title, presence: true, length: { maximum: 250 }
+  validates :CommentsCounter,  numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :LikesCounter,  numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   belongs_to :user, foreign_key: 'author_id'
   has_many :likes
