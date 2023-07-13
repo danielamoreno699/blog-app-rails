162fe88666699fad26075b2421bfce269e4efab3
Daniela Moreno <dani9292@hotmail.com>
2023-07-13 09:55:12 -0500

    Insert validations for user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+
+  
+  validates :name, presence: true
+  validates :photo, presence: true
+  validates :bio, presence: true
+
+
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
