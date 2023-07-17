55e3ecfb4388ce393ef01d479eb8408c8adf98e3
Daniela Moreno <dani9292@hotmail.com>
2023-07-17 11:41:35 -0500

    implement methods for users and implment dynamic template in views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
-end
+    def index
+      @users = User.all
+      
+    end
+  
+    def show
+        @user = User.find(params[:id])
+        respond_to do |format|
+          format.html 
+          format.json { render json: @user } 
+        end
+      end
+  end
+  
