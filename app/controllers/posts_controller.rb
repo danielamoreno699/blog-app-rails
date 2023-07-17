008d9774178474010ecd7056c0ce414594f766a8
Daniela Moreno <dani9292@hotmail.com>
2023-07-17 13:11:00 -0500

    implement views for all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
-class UserPostsController < ApplicationController
-    def index
-        @posts = Post.all
-        
-      end
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    @comments = Comment.all
+  end
+  
     
       def show
           @post = Post.find(params[:id])
