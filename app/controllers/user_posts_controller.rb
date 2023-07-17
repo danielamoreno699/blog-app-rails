ac5e3caaebe42d1990452d820f841e2590ced664
Daniela Moreno <dani9292@hotmail.com>
2023-07-17 12:28:37 -0500

    implement methods in users posts controller and created views

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -1,2 +1,14 @@
 class UserPostsController < ApplicationController
+    def index
+        @posts = Post.all
+        
+      end
+    
+      def show
+          @post = Post.find(params[:id])
+          respond_to do |format|
+            format.html 
+            format.json { render json: @post } 
+          end
+        end
 end
