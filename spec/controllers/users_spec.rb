f5ca52a05edc9e2a9cc0810de1ae8630e34a5334
Daniela Moreno <dani9292@hotmail.com>
2023-07-18 11:25:27 -0500

    returns correct template for users

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe UsersController, type: :controller do
   end
 
   describe "GET index" do
-    it "returns http success" do
+    it "returns http success and renders correct template" do
       get :index
+
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
     end
   end
 end
