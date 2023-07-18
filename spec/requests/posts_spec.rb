5ba40da43952ad2d5749bf3d8c582a6e10a01aac
Daniela Moreno <dani9292@hotmail.com>
2023-07-18 12:47:39 -0500

    create test for render  status and placeholder for single post

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe "Posts", type: :request do
       expect(response.body).to include("Here is the List of posts associated with User account")
     end
   end
+
+  describe "GET /users/:user_id/posts/:id" do
+    it "returns http success and renders correct template and displays placeholder" do
+      get "/users/1/posts/1"
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include("Here is the details of the post ID  associated with User account")
+    end
+  end
 end
