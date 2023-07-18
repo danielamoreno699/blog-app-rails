da0041e2d8d9a09c2a046c649f403325fe2e5418
Daniela Moreno <dani9292@hotmail.com>
2023-07-18 12:44:53 -0500

    create test for render  status and placeholder for list of posts on index

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /users/:user_id/posts" do
+    it "returns http success and renders correct template and displays placeholder" do
+      get "/users/1/posts"
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include("Here is the List of posts associated with User account")
+    end
   end
 end
