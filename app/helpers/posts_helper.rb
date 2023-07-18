06b45085492cb13e2677264c951359b771afa9f9
Daniela Moreno <dani9292@hotmail.com>
2023-07-18 16:16:16 -0500

    move logic of rendering comments to helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
 end
+module PostsHelper
+    def render_comments_section(post)
+      content_tag(:ul, class: 'ul-comments-box') do
+        concat content_tag(:h4, 'Section for Comments:', class: 'section-comments')
+        
+        if post.comments.blank?
+          concat content_tag(:li, 'no comments for the moment')
+        else
+          post.comments.each do |comment|
+            concat content_tag(:li, "Username: #{comment.Text.blank? ? 'no comments for the moment' : comment.Text}")
+          end
+        end
+      end
+    end
+  end
+  
