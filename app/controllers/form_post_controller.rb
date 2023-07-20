7baca8a5a5d58ff01e0872e813b9c10618318cbd
Daniela Moreno <dani9292@hotmail.com>
2023-07-20 14:52:04 -0500

    add to posts

--- app/controllers/form_post_controller.rb
+++ app/controllers/form_post_controller.rb
@@ -1,29 +1,29 @@
 class FormPostController < ApplicationController
-    def new
-      @current_user = current_user
-      @form_post = Post.new
-      respond_to do |format|
-        format.html { render :new, locals: { form_post: @form_post } }
-      end
-    end
-  
-    def create
-      @current_user = current_user
-      @post = Post.create(post_params)
-  
-      if @post.save
-        flash[:notice] = "Post was successfully created."
-        redirect_to user_post_path(user_id: @current_user.id, id: @post.id)
-      else
-        flash.now[:alert] = "Error: Post could not be created."
-        render :new
-      end
-    end
-  
-    private
-  
-    def post_params
-      params.require(:post).permit(:Title, :CommentsCounter, :LikesCounter)
+  def new
+    @current_user = current_user
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
     end
   end
-  
+
+  def create
+    @current_user = current_user
+    @post = Post.new(post_params)
+    @post.author_id = @current_user.id
+
+    if @post.save
+      flash[:success] = "Post was successfully created."
+      redirect_to users_path
+    else
+      flash.now[:alert] = "Error: Post could not be created."
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:Title, :Text, :CommentsCounter, :LikesCounter)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,16 @@ Rails.application.routes.draw do
 
     get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
 
-    # get '/users/:user_id/form_post/new', to: 'posts#new', as: 'new_form_post'
-    # post '/users/:user_id/form_post', to: 'posts#create', as: 'form_posts'
 
-    get '/users/:user_id/posts/:id/form_comment/new', to: 'form_comment#new', as: 'new_form_comment'
-    post '/users/:user_id/posts/:id/form_comment', to: 'form_comment#create', as: 'form_comments'
+    # get '/form_comment/new', to: 'form_comment#new', as: 'new_form_comment'
+    # post '/form_comment', to: 'form_comment#create', as: 'form_comment'
 
     get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
     get '/users', to: 'users#index', as: 'users'
     get '/users/:id', to: 'users#show', as: 'user'
 
-    get '/users/:user_id/form_post/new', to: 'form_post#new', as: 'new_form_post'
-    post '/users/:user_id/form_post', to: 'form_post#create', as: 'form_posts'
+    get '/form_post/new', to: 'form_post#new', as: 'new_form_post'
+    post '/form_post', to: 'form_post#create', as: 'form_posts'
   # Defines the root path route ("/")
   # root "articles#index"
 end
