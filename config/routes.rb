948553e71b4b218ec6a22a58cce28c4df493e357
Daniela Moreno <dani9292@hotmail.com>
2023-07-20 16:29:23 -0500

    update routes for form commits

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,15 @@ Rails.application.routes.draw do
 
     get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
 
-
-    # get '/form_comment/new', to: 'form_comment#new', as: 'new_form_comment'
-    # post '/form_comment', to: 'form_comment#create', as: 'form_comment'
-
     get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
     get '/users', to: 'users#index', as: 'users'
     get '/users/:id', to: 'users#show', as: 'user'
 
     get '/form_post/new', to: 'form_post#new', as: 'new_form_post'
     post '/form_post', to: 'form_post#create', as: 'form_posts'
+
+    get '/form_comment/new', to: 'form_comment#new', as: 'new_form_comment'
+    post '/form_comment', to: 'form_comment#create', as: 'form_comments'
   # Defines the root path route ("/")
   # root "articles#index"
 end
