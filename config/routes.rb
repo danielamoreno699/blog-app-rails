248904ccf6811a7407b1c0cb3ccca1369aad29b9
Daniela Moreno <dani9292@hotmail.com>
2023-07-19 20:49:33 -0500

    update routes for comemnt forms and post forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
 
 
     get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+
+    get '/users/:user_id/posts/form_post/new', to: 'posts#new', as: 'new_form_post'
+    post '/users/:user_id/posts/form_post', to: 'posts#create', as: 'form_posts'
+
+    get '/users/:user_id/posts/form_comment/new', to: 'comments#new', as: 'new_form_comment'
+    post '/users/:user_id/posts/form_comment', to: 'comments#create', as: 'form_comments'
+
     get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
     get '/users', to: 'users#index', as: 'users'
     get '/users/:id', to: 'users#show', as: 'user'
 
-    resources :users do
-      resources :posts, only: [:index, :show, :new, :create], shallow: true
-    end
+   
   # Defines the root path route ("/")
   # root "articles#index"
 end
