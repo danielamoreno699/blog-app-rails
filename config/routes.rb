35325e5d111556b2d1d2c97c01dfdfbaca40636c
Daniela Moreno <dani9292@hotmail.com>
2023-07-19 20:29:27 -0500

    add resources routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
     get '/users', to: 'users#index', as: 'users'
     get '/users/:id', to: 'users#show', as: 'user'
+
+    resources :users do
+      resources :posts, only: [:index, :show, :new, :create], shallow: true
+    end
   # Defines the root path route ("/")
   # root "articles#index"
 end
