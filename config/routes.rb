eab5b15cacccdfbbdaf983a568e50d8b086e412a
Daniela Moreno <dani9292@hotmail.com>
2023-07-20 10:30:31 -0500

    update routes for forms

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
 
     get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
 
-    get '/users/:user_id/posts/form_post/new', to: 'posts#new', as: 'new_form_post'
-    post '/users/:user_id/posts/form_post', to: 'posts#create', as: 'form_posts'
+    get '/users/:user_id/form_post/new', to: 'posts#new', as: 'new_form_post'
+    post '/users/:user_id/form_post', to: 'posts#create', as: 'form_posts'
 
-    get '/users/:user_id/form_comment/new', to: 'comments#new', as: 'new_form_comment'
-    post '/users/:user_id/form_comment', to: 'comments#create', as: 'form_comments'
+    get '/users/:user_id/posts/:id/form_comment/new', to: 'comments#new', as: 'new_form_comment'
+    post '/users/:user_id/posts/:id/form_comment', to: 'comments#create', as: 'form_comments'
 
     get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
     get '/users', to: 'users#index', as: 'users'
