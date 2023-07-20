3f37ec480f08c9cc9534a7fb3120702a35b3d33b
Daniela Moreno <dani9292@hotmail.com>
2023-07-19 21:06:40 -0500

    update form post controller with new and create method on behalf of current user

--- app/controllers/form_posts_controller.rb
+++ app/controllers/form_posts_controller.rb
@@ -1,11 +1,26 @@
 class FormPostsController < ApplicationController
-
     def new
-        @post = Post.new
+      @current_user = current_user
+      @post = Post.new
     end
-
+  
     def create
-        @post = Post.create(post_params)
-        redirect_to user_posts_path
+      @current_user = current_user
+      @post = Post.create(post_params)
+  
+      if @post.save
+        flash[:notice] = "Post was successfully created."
+        redirect_to user_post_path(user_id: @current_user.id, id: @post.id)
+      else
+        flash.now[:alert] = "Error: Post could not be created."
+        render :new
+      end
     end
-end
+  
+    private
+  
+    def post_params
+      params.require(:post).permit(:Title, :CommentsCounter, :LikesCounter)
+    end
+  end
+  
