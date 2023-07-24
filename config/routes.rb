898af7ba94c3c1eec1afaba42c61b320bc39c534
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-07-25 02:29:20 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-
+    root 'users#index'
     get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
 
     get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
