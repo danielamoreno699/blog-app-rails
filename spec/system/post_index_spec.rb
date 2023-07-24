85c668a44050d42f2c80672138f2d6b33b60428c
Daniela Moreno <dani9292@hotmail.com>
2023-07-24 13:33:08 -0500

    add test for post show, see the post title

--- spec/system/post_index_spec.rb
+++ spec/system/post_index_spec.rb
@@ -2,12 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe "User Posts", type: :system do
+RSpec.describe "Post Index", type: :system do
   
+  before do
+    driven_by(:rack_test)
+  end
+
   user = User.create(name: "John Doe", photo: "https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80", post_counter: 5)
 
   it "displays the user's profile picture on user posts page" do
-    #user = User.create(name: "John Doe", photo: "https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80", post_counter: 5)
+  
     visit user_posts_path(user_id: user.id)
 
    
---
+++ spec/system/post_show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Post Show", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  user = User.create(name: "John Doe", photo: "https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-4.0.3&amp;ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80", post_counter: 5)
+  post = Post.create(Title: "My First Post", Text: "This is my first post", author_id: user.id, CommentsCounter: 0, LikesCounter: 0)
+  comment = Comment.create(Text: "This is my first comment", author_id: user.id, post_id: post.id)
+
+  it "displays posts title" do 
+    visit user_post_path(user_id: user.id, id: post.id)
+    expect(page).to have_content(post.Title)
+  end
+end
