442bfeb8f23f72489762c1dea0816ad47956650d
Daniela Moreno <dani9292@hotmail.com>
2023-07-24 22:24:14 -0500

    create a col named has secured password for users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_031540) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_032143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_031540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "has_secure_password"
   end
 
   add_foreign_key "comments", "posts"
