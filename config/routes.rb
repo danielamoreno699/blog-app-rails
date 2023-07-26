f9bea984ae40676309566ae9ecc0ba160a10c445
Daniela Moreno <dani9292@hotmail.com>
2023-07-26 12:01:28 -0500

    add application controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,23 @@ Rails.application.routes.draw do
  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_for :users, controllers: {
-  registrations: 'users/registrations'
-}
+#   devise_for :users, controllers: {
+#   registrations: 'users/registrations',
+#   sessions: 'users/sessions'
 
-    #devise_for :users
+# }
+
+
+    devise_for :users
     
 
     root 'users#index'
+    # resources :users, only: [:index, :show] do
+    #   resources :posts, only: [:index, :show, :new, :create] do
+    #     resources :comments, only: [:new, :create]
+    #     resources :likes, only: [:new, :create]
+    #   end
+    # end
     get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
 
     get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
