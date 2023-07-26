b52f82f383579ca94ef18ce41ce4f786cb517242
Daniela Moreno <dani9292@hotmail.com>
2023-07-26 16:27:29 -0500

    add css style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # respond_to do |format|
-    #   format.html
-    #   format.json { render json: @user }
-    # end
+ 
   end
 
 end
