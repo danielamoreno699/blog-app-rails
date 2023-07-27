8a1a3da43847b6b0e0015e03e5e5706863f7ccab
Daniela Moreno <dani9292@hotmail.com>
2023-07-27 13:55:05 -0500

    add photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :password_confirmation, :name, :bio)
+      u.permit(:name, :email, :password, :password_confirmation, :name, :bio :photo)
     end
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password) }
   end
