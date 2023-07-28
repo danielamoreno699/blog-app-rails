3fc1db4a5db95a4239af89d5a8bd51bc8ff8b9c6
Daniela Moreno <dani9292@hotmail.com>
2023-07-28 16:56:36 -0500

    generate controller for apis

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
+
+
     devise_for :users
    
    
